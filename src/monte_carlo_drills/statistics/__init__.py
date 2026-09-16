242ec9c27f93d58069d7bd33522e90dbd39467a1
Brice Nelson <bnelsonemail@icloud.com>
2026-09-16 00:34:47 -0400

    feat: update helper function and baseline notebook

---
+++ src/monte_carlo_drills/statistics/__init__.py
@@ -0,0 +1,5 @@
+"""Statistics helpers."""
+
+from .summary_statistics import summary_statistics
+
+__all__ = ["summary_statistics"]
--- src/monte_carlo_drills/statistics/summary_statistics.py
+++ src/monte_carlo_drills/statistics/summary_statistics.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 
-def  summary_statistics(df: pd.DataFrame) -> pd.DataFrame:
+def summary_statistics(df: pd.DataFrame) -> pd.DataFrame:
     """Calculates summary statistics"""
     summary = {
         "minimum": df["peak_flow"].min(),
@@ -13,4 +13,3 @@ def  summary_statistics(df: pd.DataFrame) -> pd.DataFrame:
     }
 
     return pd.DataFrame([summary])
-    
