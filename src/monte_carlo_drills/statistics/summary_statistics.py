9f18fb5c16737b85f151ef0ad957ce63054aa417
Brice Nelson <bnelsonemail@icloud.com>
2026-09-16 00:03:54 -0400

    feat: addition of statistics function helpers

---
+++ src/monte_carlo_drills/statistics/summary_statistics.py
@@ -0,0 +1,16 @@
+"""Helper functions for summary statistics"""
+
+import pandas as pd
+
+
+def  summary_statistics(df: pd.DataFrame) -> pd.DataFrame:
+    """Calculates summary statistics"""
+    summary = {
+        "minimum": df["peak_flow"].min(),
+        "maximum": df["peak_flow"].max(),
+        "mean": df["peak_flow"].mean(),
+        "median": df["peak_flow"].median()
+    }
+
+    return pd.DataFrame([summary])
+    
